307b300ea06f577874d205768061c8f167a279be
austin h <athleticatxbox2@gmail.com>
2021-10-07 12:57:32 -0700

    finale prject

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 import random
 
-destinations=["Hawaii","Fiji","Taiwon","Japan"]
-transportations=["Plane","Boat","Cruise","Private Jet"]
-resturaunts=["Red lobster","Chilis","Sushi Bar","Pho"]
-entertainments=["Fishing","Surfing","Snorkling","Whale watching"]
-
-
-def random_trip(entertainment,resturaunt,transportation,destination): 
-    finale_trip=destination+transportation+resturaunt+entertainment
-
-
-
-Locked=True
-
-while Locked is True:
-    print("Type Go! to generate random trip")
-    Key= input("")
-    if Key =="Go!":
-        Locked=False
-        destination= random.choice(destinations)
-        transportation=random.choice(transportations)
-        resturaunt=random.choice(resturaunts)
-        entertainment=random.choice(entertainments)
-        print(f"You will be going to {destination} .You will take a {transportation} ,then eat {resturaunt} ending your day with {entertainment}")
-    else:
-        Locked=True
-while Locked is False:
-    print("Is this trip satisfying?")
-    confirmation=input(" ")
-    if confirmation=="Yes":
-        print("Your trip has been confirmed.")
-        print(f"You will be going to {destination} .You will take a {transportation} ,then eat {resturaunt} ending your day with {entertainment}")
-        break
-    else:Locked=True
-
+place_to_go=["Hawaii","Fiji","Taiwan","Japan"]
+mobility=["Plane","Boat","Cruise","Private Jet"]
+place_to_eat=["Red lobster","Chilis","Sushi Bar","Pho"]
+stuff_to_do=["Fishing","Surfing","Snorkling","Whale watching"]
+            #Function that generates a random vacation with confirmation.
+def random_trip(destination,transportation,resturaunt,entertainment): 
+    Locked=True
+    while Locked is True:
+        print("Type Go! to generate random trip")
+        Key= input("")
+        if Key =="Go!":    
+             #Generate randoms from list above.
+            destinations= random.choice(destination)
+            transportations=random.choice(transportation)
+            resturaunts=random.choice(resturaunt)
+            entertainments=random.choice(entertainment)
+            print(f"You will be going to {destinations}.You will take a {transportations},then eat {resturaunts} ending your day with {entertainments}.")
+        
+            #Confirmation that trip is good or not/loops back if not.
+            print("Is this trip satisfying?")
+            confirmation=input(" ")
+            if confirmation=="Yes":
+                print("Your trip has been confirmed.")
+                print(f"You will be going to {destinations}.You will take a {transportations},then eat {resturaunts} ending your day with {entertainments}.")
+                break
+            else:Locked=True
+random_trip(place_to_go,mobility,place_to_eat,stuff_to_do)
     
             
 
